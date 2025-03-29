f366eb4379e877ac9c4a60446a86c5b775c6b751
KAI-005 <shreyansh6410@gmail.com>
2025-03-29 20:55:51 +0530

    checkpoint 2.0

--- Shreyansh/reference.py
+++ Shreyansh/reference.py
@@ -1,5 +1,29 @@
+import pandas as pd
+#import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.metrics import accuracy_score, mean_absolute_error
+path = r'C:\Users\anshv\OneDrive - Manipal Academy of Higher Education\Documents\GitHub\indigrow\Shreyansh\updated_india_agriculture_dataset.csv'
+try:
+    df=pd.read_csv(path)
+except FileNotFoundError:
+    print("File not Found")
+#columns=['State','Crop','Soil','District','Irrigation Method','Best Next Crop']
+columns=["State","Soil Type","Crop","District","Best Fertilizer","Alternative Fertilizer"]
+for col in columns:
+    if col not in df.columns:
+        print(f"Column {col} is not in the dataset")
+encoder=LabelEncoder()
+#for col in ['State', 'District', 'Soil', 'Crop', 'Irrigation Method', 'Best Next Crop']:
+for col in ["State","Soil Type","Crop","District","Best Fertilizer","Alternative Fertilizer"]:
+    df[col] = encoder.fit_transform(df[col])
 
-#model 4(Shresh's model)
+
+x= df[['State','District','Crop','Soil Type']]
+
+
+#model 3(Shresh's model)
 
 y_predictor_fert=df['Best Fertilizer']
 x_train,x_test,y_train,y_test=train_test_split(x,y_predictor_fert,test_size=0.2,random_state=42)
